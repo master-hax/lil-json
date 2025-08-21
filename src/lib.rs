a7ad9205db0d905d0b64157cc5ac47788585e8e2
Vivek Revankar <vivek@master-hax.com>
2025-08-20 23:52:03 -0700

    refactor + documentation

--- src/lib.rs
+++ src/lib.rs
@@ -40,14 +40,12 @@ impl <'a,'b> JsonField<'a,'b> {
     }
 }
 
-/// a JSON Object (rfc8259) that wraps a type that implements a buffer of object fields. The easiest way to use it is through the ArrayJsonObject type alias, however you can use JsonObject directly to wrap your own buffer (maybe a Vec)
+/// a JSON Object (rfc8259) that wraps a mutable or immutable buffer of object fields. The easiest way to use it is through the ArrayJsonObject type alias, however you can use JsonObject directly to wrap your own buffer (maybe a Vec)
 #[derive(Debug)]
 pub struct JsonObject<Fields> {
     fields: Fields,
     num_fields: usize,
 }
-
-
 
 /// the various reasons parsing JSON can fail
 #[derive(Debug)]
@@ -81,7 +79,9 @@ impl<'a,T: FieldBuffer<'a> + Default> Default for JsonObject<T> {
     }
 }
 
+/// this trait is automatically implemented for all types that implement AsRef<[JsonField<'data,'data>]>
 pub trait FieldBuffer<'data>: AsRef<[JsonField<'data,'data>]> {}
+/// this trait is automatically implemented for all types that implement AsMut<[JsonField<'data,'data>]>
 pub trait FieldBufferMut<'a>: FieldBuffer<'a> +  AsMut<[JsonField<'a,'a>]> {}
 
 impl <'a,T: AsRef<[JsonField<'a,'a>]>> FieldBuffer<'a> for T {}
@@ -164,12 +164,12 @@ pub type ArrayJsonObject<'a,const N: usize> = JsonObject<[JsonField<'a,'a>; N]>;
 
 impl<'a,const N: usize> ArrayJsonObject<'a,N> {
 
-    /// create a new empty JsonObject backed by an array that holds at most N object member fields
+    /// convenience method to wrap a JsonObject over an array
     pub const fn new() -> Self {
         JsonObject::wrap([EMPTY_FIELD; N], 0)
     }
 
-    /// the similar to JsonObject::push but supports const contexts & only returns a reference
+    /// similar to JsonObject::push but supports const contexts & only returns a reference
     pub const fn push_const(&mut self, key: &'a str, value: JsonValue<'a>) -> Result<(),()> {
         if self.num_fields == N {
             return Err(());
@@ -179,7 +179,7 @@ impl<'a,const N: usize> ArrayJsonObject<'a,N> {
         Ok(())
     }
 
-        /// the same as JsonObject::push but supports const contexts
+    /// similar to JsonObject::pop but supports const contexts
     pub const fn pop_const(&mut self) -> Option<&JsonField<'a,'a>> {
         match self.fields_const().split_last() {
             None => return None,
@@ -187,80 +187,26 @@ impl<'a,const N: usize> ArrayJsonObject<'a,N> {
         }
     }
 
+    /// convenience method to automatically create an ArrayJsonObject if object parsing is successful
     pub fn new_parsed(data: &'a [u8]) -> Result<(usize,Self),JsonParseFailure> {
         let mut ret = Self::new();
         let (data_end,num_headers) = ret.parse(data)?;
         Ok((data_end,ret))
     }
 
+    /// same as JsonObject::fields but supports const contexts
     pub const fn fields_const(&self) -> &[JsonField<'a,'a>] {
         self.fields.split_at(self.num_fields).0
     }
 
+    /// same as JsonObject::fields_mut but supports const contexts
     pub const fn fields_mut_const(&mut self) -> &mut [JsonField<'a,'a>] {
         self.fields.split_at_mut(self.num_fields).0
     }
 
 }
 
-
-fn skip_json_string(index: &mut usize, data: &[u8]) -> Result<(),JsonParseFailure> {
-    let mut last_char_escape = false;
-    while *index < data.len() {
-        if data[*index] == b'\\' && !last_char_escape {
-            last_char_escape = true;
-        } else if data[*index] == b'"' && !last_char_escape {
-            return Ok(());
-        } else if !data[*index].is_ascii() {
-            return Err(JsonParseFailure::InvalidStringField);
-        } else {
-            last_char_escape = false
-        }
-        *index += 1;
-    }
-    Err(JsonParseFailure::Incomplete)
-}
-
-fn skip_json_numeric(index: &mut usize, data: &[u8]) -> Result<(),JsonParseFailure> {
-    while *index < data.len() && data[*index] <= b'9' && data[*index] >= b'0' {
-        *index += 1;
-    }
-    if *index == data.len() {
-        Err(JsonParseFailure::Incomplete)
-    } else if data[*index].is_ascii_whitespace() || data[*index] == b',' || data[*index] == b'}' {
-        Ok(())
-    } else {
-        Err(JsonParseFailure::InvalidNumericField)
-    }
-}
-
-fn skip_json_boolean(index: &mut usize, data: &[u8], value: bool) -> Result<(),JsonParseFailure> {
-    let start = *index;
-    let target = if value { "true" } else { "false" };
-    while (*index - start) < target.len() {
-        if *index >= data.len() {
-            return Err(JsonParseFailure::Incomplete)
-        }
-        if data[*index] != target.as_bytes()[*index-start] {
-            return Err(JsonParseFailure::InvalidBooleanField);
-        }
-        *index += 1;
-    }
-    Ok(())
-}
-
-fn skip_whitespace(index: &mut usize, data: &[u8]) -> Result<(),JsonParseFailure> {
-    while *index < data.len() && data[*index].is_ascii_whitespace() {
-        *index += 1;
-    }
-    if *index == data.len() {
-        Err(JsonParseFailure::Incomplete)
-    } else {
-        Ok(())
-    }
-}
-
-/// the core function that powers the JsonObject API. It attempts to parse the fields of a json object from the provided data slice into the provided field buffer, then return (data bytes consumed, parsed field slice) on success.
+/// the core function that powers parsing in the JsonObject API. It attempts to parse the fields of a json object from the provided data slice into the provided field buffer, then return (data bytes consumed, parsed field slice) on success.
 pub fn parse_json_object<'data,'field_buffer>(data: &'data [u8], field_buffer: &'field_buffer mut [JsonField<'data,'data>]) -> Result<(usize,&'field_buffer[JsonField<'data,'data>]),JsonParseFailure> {
     let mut current_data_index = 0;
     let mut current_field_index = 0;
@@ -339,26 +285,63 @@ pub fn parse_json_object<'data,'field_buffer>(data: &'data [u8], field_buffer: &
     Err(JsonParseFailure::Incomplete)
 }
 
-fn tracked_write<T: Write>(mut output: T, counter: &mut usize, data: &str) -> Result<(), T::Error> {
-    output.write_all(data.as_bytes())?;
-    *counter += data.len();
-    Ok(())
-}
-
-fn write_escaped_json_string<T: Write>(mut output: T, counter: &mut usize, data: &str) -> Result<(), T::Error> {
-    tracked_write(&mut output, &mut *counter, "\"")?;
-    for field_character in data.chars() {
-        if field_character == '"' {
-            tracked_write(&mut output, &mut *counter, unsafe { core::str::from_utf8_unchecked(&[b'\\', field_character as u8]) })?;
+fn skip_json_string(index: &mut usize, data: &[u8]) -> Result<(),JsonParseFailure> {
+    let mut last_char_escape = false;
+    while *index < data.len() {
+        if data[*index] == b'\\' && !last_char_escape {
+            last_char_escape = true;
+        } else if data[*index] == b'"' && !last_char_escape {
+            return Ok(());
+        } else if !data[*index].is_ascii() {
+            return Err(JsonParseFailure::InvalidStringField);
         } else {
-            tracked_write(&mut output, &mut *counter, unsafe { core::str::from_utf8_unchecked(&[field_character as u8]) })?;
+            last_char_escape = false
         }
+        *index += 1;
     }
-    tracked_write(&mut output, &mut *counter, "\"")?;
+    Err(JsonParseFailure::Incomplete)
+}
+
+fn skip_json_numeric(index: &mut usize, data: &[u8]) -> Result<(),JsonParseFailure> {
+    while *index < data.len() && data[*index] <= b'9' && data[*index] >= b'0' {
+        *index += 1;
+    }
+    if *index == data.len() {
+        Err(JsonParseFailure::Incomplete)
+    } else if data[*index].is_ascii_whitespace() || data[*index] == b',' || data[*index] == b'}' {
+        Ok(())
+    } else {
+        Err(JsonParseFailure::InvalidNumericField)
+    }
+}
+
+fn skip_json_boolean(index: &mut usize, data: &[u8], value: bool) -> Result<(),JsonParseFailure> {
+    let start = *index;
+    let target = if value { "true" } else { "false" };
+    while (*index - start) < target.len() {
+        if *index >= data.len() {
+            return Err(JsonParseFailure::Incomplete)
+        }
+        if data[*index] != target.as_bytes()[*index-start] {
+            return Err(JsonParseFailure::InvalidBooleanField);
+        }
+        *index += 1;
+    }
     Ok(())
 }
 
-/// serialize a json object with the provided fields into output & return the number of bytes written on success
+fn skip_whitespace(index: &mut usize, data: &[u8]) -> Result<(),JsonParseFailure> {
+    while *index < data.len() && data[*index].is_ascii_whitespace() {
+        *index += 1;
+    }
+    if *index == data.len() {
+        Err(JsonParseFailure::Incomplete)
+    } else {
+        Ok(())
+    }
+}
+
+/// the core function that powers serialization in the JsonObject API. It attempts to serialize the provided fields as a JSON object into the provided output, & returns the number of bytes written on success.
 pub fn serialize_json_object<'data,Output: Write, Fields: FieldBuffer<'data>>(mut output: Output, fields: Fields) -> Result<usize, Output::Error> {
     let mut ret = 0;
     tracked_write(&mut output,&mut ret , "{")?;
@@ -390,6 +373,25 @@ pub fn serialize_json_object<'data,Output: Write, Fields: FieldBuffer<'data>>(mu
     Ok(ret)
 }
 
+
+fn tracked_write<T: Write>(mut output: T, counter: &mut usize, data: &str) -> Result<(), T::Error> {
+    output.write_all(data.as_bytes())?;
+    *counter += data.len();
+    Ok(())
+}
+
+fn write_escaped_json_string<T: Write>(mut output: T, counter: &mut usize, data: &str) -> Result<(), T::Error> {
+    tracked_write(&mut output, &mut *counter, "\"")?;
+    for field_character in data.chars() {
+        if field_character == '"' {
+            tracked_write(&mut output, &mut *counter, unsafe { core::str::from_utf8_unchecked(&[b'\\', field_character as u8]) })?;
+        } else {
+            tracked_write(&mut output, &mut *counter, unsafe { core::str::from_utf8_unchecked(&[field_character as u8]) })?;
+        }
+    }
+    tracked_write(&mut output, &mut *counter, "\"")?;
+    Ok(())
+}
 
 #[cfg(test)]
 mod tests {
