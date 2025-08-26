c37e5266004eae8060f118220174785361d01ac0
Vivek Revankar <vivek@master-hax.com>
2025-08-26 01:05:15 -0700

    nits

--- examples/proxy_object_std.rs
+++ examples/proxy_object_std.rs
@@ -4,7 +4,7 @@ use std::{io::{stderr, stdin, stdout}, process::exit};
 
 use embedded_io::{Read, Write};
 use embedded_io_adapters::std::FromStd;
-use lil_json::{ArrayJsonObject, JsonField, JsonParseFailure, JsonValue};
+use lil_json::{ArrayJsonObject, JsonParseFailure};
 
 const MAX_FIELDS: usize = 50;
 const READ_BUFFER_SIZE: usize = 16384; // 16 KiB
@@ -32,22 +32,26 @@ fn proxy_json_object<Input: Read, Output: Write, Logs: Write>(mut input: Input, 
             Ok(n) => {
                 read_buffer_end += n;
                 match ArrayJsonObject::<MAX_FIELDS>::new_parsed(read_buffer.split_at(read_buffer_end).0, &mut escape_buffer) {
-                    Err(JsonParseFailure::Incomplete) => continue,
+                    Err(JsonParseFailure::Incomplete) => {
+                        if read_buffer_end >= READ_BUFFER_SIZE {
+                            log_output.write_fmt(format_args!("failed to parse json object after {} bytes\n", read_buffer_end)).unwrap();
+                            exit(1);
+                        }
+                        continue;
+                    },
                     Err(e) => {
                         log_output.write_fmt(format_args!("read {} bytes, failed to parse json object: {:?}\n", read_buffer_end, e)).unwrap();
-                        log_output.flush().unwrap();
                         exit(1);
                     },
                     Ok((bytes_consumed, json_object)) => {
                         log_output.write_fmt(format_args!("read {} bytes, parsed a json object in {} bytes with {} fields\n", read_buffer_end, bytes_consumed, json_object.len())).unwrap();
-                        log_output.flush().unwrap();
                         json_object.serialize(&mut output).unwrap();
-                        output.flush().unwrap();
                         exit(0)
                     },
                 }
             },
 
         }
+
     }
 }
